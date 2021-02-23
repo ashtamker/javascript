7280ea435bcd1448e7b9d1caa2b29775424148d9
Or Ashtamker <ashi@MacBook-Pro.local>
2021-02-24 00:59:38 +0200

    add 19 finish

--- 19-2/main.js
+++ 19-2/main.js
@@ -1,4 +1,27 @@
-const target = document.querySelector('div.input');
+const form = document.querySelector('[name="verify"]');
+const target = document.querySelectorAll('.text-box input');
+
+function getInput(e){
+    const input = e.target;
+    if(input.nextElementSibling && input.value){
+        input.nextElementSibling.focus();
+    }
+}
+
+function getPaste(e) {
+   const textPaste = e.clipboardData.getData('text');
+   target.forEach((input, i) => {
+       input.value = textPaste[i] || '';
+       
+   }); 
+}
+
+target[0].addEventListener('paste', getPaste)
+form.addEventListener('input', getInput)
+
+
+
+
 
 
 $('input.digFocus').keyup(function(){
