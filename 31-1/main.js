2609c3441e2d33c95b4cd4e6b789933a8f432e32
Or Ashtamker <ashi@MacBook-Pro.local>
2021-03-01 22:37:59 +0200

    31-1

--- 31-1/main.js
+++ 31-1/main.js
@@ -22,6 +22,20 @@ const getRecipe = (recipeID) => {
   }); 
 };
 
+
+getIDs()
+    .then((IDs) => {
+        console.log(IDs);
+        return getRecipe(IDs[2]);
+    })
+    .then((recipe) => {
+        console.log(recipe);
+    })
+    .catch((error) => {
+        console.log("It is an error!");
+    });
+
+
   const run = async () => {
     const listId = await getIDs();
     const recipe = await getRecipe(listId[0]);
